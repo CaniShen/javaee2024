74b54f86a288340755aa5cd8aa99a30d60d9aa9c
CaniShen <164502320+CaniShen@users.noreply.github.com>
2024-07-09 15:47:58 +0200

    09-07-2024 DOUBLE CHECK!

--- 05_tabla_numero/src/main/java/servlets/TablaMultiplicar.java
+++ 05_tabla_numero/src/main/java/servlets/TablaMultiplicar.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/TablaMultiplicar ")
+@WebServlet("/TablaNumero")
 public class TablaMultiplicar extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
