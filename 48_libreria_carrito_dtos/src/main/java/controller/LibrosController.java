369c096e1dbf72221d0e4a0181f1f073cfcf0c51
CaniShen <164502320+CaniShen@users.noreply.github.com>
2024-07-09 17:12:29 +0200

    Double Check (casa)OK!

--- 48_libreria_carrito_dtos/src/main/java/controller/LibrosController.java
+++ 48_libreria_carrito_dtos/src/main/java/controller/LibrosController.java
@@ -15,7 +15,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import model.Libro;
 import service.LibrosService;
 
-@WebServlet("/LibrosAction")
+@WebServlet("/LibrosController")
 public class LibrosController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
--- 50_libreria_login_registro_cdi/src/main/java/controller/LibrosController.java
+++ 50_libreria_login_registro_cdi/src/main/java/controller/LibrosController.java
@@ -13,10 +13,9 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import model.Libro;
 import service.LibrosService;
 
-@WebServlet("/LibrosAction")
+@WebServlet("/LibrosController")
 public class LibrosController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	@Inject
